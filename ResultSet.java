6389b911eeea85afa4a6c4f1e0706146c3f4f000
govind22 <govindkalyankar22@gmail.com>
2014-03-04 09:39:41 -0800

    Create ResultSet.java

---
+++ ResultSet.java
@@ -0,0 +1,10 @@
+
+public class ResultSet 
+{	
+	/**
+	 This class us used to act like a structure in C/C++ language which stores docid and score.
+	 In boolean query and cosine similarity arrayList of this class is used to store the result.
+	 */
+   int docid;
+   double score;
+}
